18d275a4eb0c4a607cef0ca47e857c1dc89afd60
VotreNomUtilisateur <votre@email.com>
2024-11-20 16:21:21 +0100

    Ajout du nom de la ville de départ et d'arrivée

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:async'; // Import for using Timer
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_svg/flutter_svg.dart'; // Import the flutter_svg package
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,58 +20,40 @@ class MyApp extends StatelessWidget {
         colorSchemeSeed: Colors.blue,
         textTheme: TextTheme(
           titleLarge: TextStyle(
-            fontSize: 22.0, // Adjust according to Material 3 guidelines
+            fontSize: 22.0,
             fontWeight: FontWeight.w600,
           ),
         ),
       ),
       home: Scaffold(
-        backgroundColor: Color(0xFF002157), // Set the background color here
+        backgroundColor: Color(0xFF002157),
         appBar: AppBar(
           centerTitle: true,
           title: Row(
-            mainAxisAlignment: MainAxisAlignment.start, // Align items to the start
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              // Leading Menu Button
               IconButton(
                 icon: Icon(Icons.menu),
-                onPressed: () {
-                  // Handle menu action
-                },
+                onPressed: () {},
               ),
-              // Add the logo next to the menu button
               Container(
-                margin: EdgeInsets.only(left: 8.0, right: 8.0), // Add margin for spacing
+                margin: EdgeInsets.symmetric(horizontal: 8.0),
                 child: SvgPicture.asset(
-                  'assets/Air_France_Logo.svg', // Path to your SVG logo
-                  fit: BoxFit.contain, // Adjust the image fitting
-                  height: 20, // Set the height of the logo
+                  'assets/Air_France_Logo.svg',
+                  fit: BoxFit.contain,
+                  height: 20,
                 ),
               ),
-              // Title text
               Spacer(),
-              Text(
-                'Air France Airplanes Map',
-                style: TextStyle(fontSize: 20), // Adjust title text size if needed
-              ),
+              Text('Air France Airplanes Map', style: TextStyle(fontSize: 20)),
               Spacer(),
             ],
           ),
           elevation: 0,
           toolbarHeight: 80,
           actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // Handle search action
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {
-                // Handle settings action
-              },
-            ),
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
           ],
         ),
         body: AirplanesMap(),
@@ -86,14 +68,13 @@ class AirplanesMap extends StatefulWidget {
 }
 
 class _AirplanesMapState extends State<AirplanesMap> {
+  final String openSkyUsername = 'luap';
+  final String openSkyPassword = 'Luapk989#';
+  final String openCageApiKey = '410626e2ecdb40ecad917ea98f71a8be';
+
   List<Marker> airplaneMarkers = [];
   double zoomLevel = 2.0;
   Timer? timer;
-  Map<String, bool> hoverStates = {}; // Store hover states
-
-  // Replace with your OpenSky username and password
-  final String username = 'AzizPistol';
-  final String password = 'Ce@Pt37sgNdSiWu  ';
 
   @override
   void initState() {
@@ -109,91 +90,186 @@ class _AirplanesMapState extends State<AirplanesMap> {
   }
 
   double _calculateIconSize(double zoomLevel, double? altitude) {
-    // Adjust size based on zoom level
-    double baseSize = zoomLevel * 3;
-
-    // Optionally adjust based on altitude
-    if (altitude != null) {
-      baseSize = baseSize * (1 + (altitude / 10000)); // Tweak the divisor to control sensitivity
-    }
-
-    // Clamp the size to prevent it from getting too small or too large
-    return baseSize.clamp(10.0, 80.0);
+    double baseSize = 10 + (zoomLevel * 2);
+    if (altitude != null) baseSize += altitude / 10000;
+    return baseSize.clamp(8.0, 30.0);
   }
 
-  // Function to fetch airplane data with Basic Authentication
   Future<void> fetchAirplanes() async {
-    final String url = 'https://opensky-network.org/api/states/all';
+    final urlOpenSky = 'https://opensky-network.org/api/states/all';
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$openSkyUsername:$openSkyPassword'));
 
-    // Encode the username and password as Base64 for Basic Authentication
-    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$username:$password'));
+    try {
+      final response = await http.get(
+        Uri.parse(urlOpenSky),
+        headers: {'Authorization': basicAuth},
+      );
 
-    final response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Authorization': basicAuth,  // Add the Authorization header
-      },
-    );
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        List<Marker> markers = [];
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      List<Marker> markers = [];
+        for (var airplane in data['states']) {
+          String? icao24 = airplane[0]?.toLowerCase(); // Convertir en minuscule
+          String? callsign = airplane[1];
+          double? lat = airplane[6];
+          double? lng = airplane[5];
+          double? heading = airplane[10];
+          double? altitude = airplane[13];
 
-      for (var airplane in data['states']) {
-        double? lat = airplane[6];
-        double? lng = airplane[5];
-        String? callsign = airplane[1];
-        double? heading = airplane[10];
-        double? altitude = airplane[13];
-        double? velocity = airplane[9];
-
-        if (callsign != null && callsign.startsWith("AFR")) {
-          if (lat != null && lng != null && heading != null) {
+          if (callsign != null && callsign.startsWith("AFR") && icao24 != null) {
             markers.add(
               Marker(
-                width: 40.0,
-                height: 40.0,
-                point: LatLng(lat, lng),
-                builder: (ctx) {
-                  bool isHovered = hoverStates[callsign] ?? false; // Check hover state
-                  Color markerColor = isHovered ? Color(0xFF931116) : Color(0xFF002157); // Change color on hover
-
-                  return MouseRegion(
-                    onEnter: (_) {
-                      setState(() {
-                        hoverStates[callsign] = true; // Set hover state to true
-                      });
-                    },
-                    onExit: (_) {
-                      setState(() {
-                        hoverStates[callsign] = false; // Set hover state to false
-                      });
-                    },
-                    child: Transform.rotate(
-                      angle: heading * (3.14159 / 180), // Rotate based on heading (convert degrees to radians)
-                      child: Tooltip(
-                        message: 'Vol numéro : $callsign\nVitesse : ${velocity?.toStringAsFixed(2) ?? 'N/A'} m/s \nAltitude : $altitude m',
-                        child: IconButton(
-                          icon: Icon(Icons.airplanemode_active, color: markerColor, size: _calculateIconSize(zoomLevel, altitude)),
-                          onPressed: () {
-                            // Handle on press if needed
-                          },
-                        ),
-                      ),
+                width: 60.0,
+                height: 60.0,
+                point: LatLng(lat ?? 0, lng ?? 0),
+                builder: (ctx) => GestureDetector(
+                  onTap: () => fetchFlightDetails(context, icao24),
+                  child: Transform.rotate(
+                    angle: heading != null ? heading * (3.14159 / 180) : 0.0,
+                    child: Icon(
+                      Icons.airplanemode_active,
+                      color: Color(0xFF002157),
+                      size: _calculateIconSize(zoomLevel, altitude),
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
             );
           }
         }
-      }
 
-      setState(() {
-        airplaneMarkers = markers;
-      });
-    } else {
-      throw Exception('Failed to load airplanes');
+        setState(() {
+          airplaneMarkers = markers;
+        });
+      } else {
+        print("Failed to fetch airplanes: ${response.body}");
+      }
+    } catch (e) {
+      print("Error fetching airplanes: $e");
+    }
+  }
+
+  Future<Map<String, dynamic>> getFlightPositions(String icao24) async {
+    final url = 'https://opensky-network.org/api/tracks/all?icao24=$icao24&time=0';
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$openSkyUsername:$openSkyPassword'));
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {'Authorization': basicAuth},
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['path'] != null && data['path'].isNotEmpty) {
+          final departure = data['path'].first;
+          final arrival = data['path'].last;
+
+          return {
+            'departure': {'lat': departure[1], 'lng': departure[2]},
+            'arrival': {'lat': arrival[1], 'lng': arrival[2]},
+          };
+        }
+      } else {
+        print("Failed to fetch flight positions: ${response.body}");
+      }
+    } catch (e) {
+      print("Error fetching flight positions: $e");
+    }
+
+    return {};
+  }
+
+  Future<void> fetchFlightDetails(BuildContext context, String icao24) async {
+    final positions = await getFlightPositions(icao24);
+
+    if (positions.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Détails de l\'avion'),
+          content: Text('Aucune donnée trouvée pour cet avion.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Fermer'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    final departureLocation = await getLocationDetails(
+      positions['departure']['lat'],
+      positions['departure']['lng'],
+    );
+    final arrivalLocation = await getLocationDetails(
+      positions['arrival']['lat'],
+      positions['arrival']['lng'],
+    );
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Détails de l\'avion'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('ICAO24: $icao24'),
+            Text('Lieu de départ : $departureLocation'),
+            Text('Lieu d\'arrivée : $arrivalLocation'),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Fermer'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<String> getLocationDetails(double lat, double lng) async {
+    final url =
+        'https://api.opencagedata.com/geocode/v1/json?q=$lat+$lng&key=$openCageApiKey';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['results'] != null && data['results'].isNotEmpty) {
+          final components = data['results'][0]['components'];
+          final city = components['city'] ??
+              components['town'] ??
+              components['village'] ??
+              components['hamlet'];
+          final road = components['road'];
+          final state = components['state'] ?? components['country'];
+
+          if (city != null) {
+            return city;
+          } else if (road != null && state != null) {
+            return '$road, $state';
+          } else if (state != null) {
+            return state;
+          } else {
+            return "Inconnu";
+          }
+        } else {
+          return "Inconnu";
+        }
+      } else {
+        return "Erreur";
+      }
+    } catch (e) {
+      return "Erreur";
     }
   }
 
@@ -217,10 +293,9 @@ class _AirplanesMapState extends State<AirplanesMap> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              // Flight Search Text Field
               TextField(
                 decoration: InputDecoration(
-                  labelText: 'Flight Search',
+                  labelText: 'Recherche vol',
                   labelStyle: TextStyle(color: Color(0xFF002157)),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
@@ -231,10 +306,10 @@ class _AirplanesMapState extends State<AirplanesMap> {
               ),
               SizedBox(height: 24),
 
-              // Flight Company Text Field
+              // Origin Field
               TextField(
                 decoration: InputDecoration(
-                  labelText: 'Flight Company',
+                  labelText: 'Origin',
                   labelStyle: TextStyle(color: Color(0xFF002157)),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
@@ -245,7 +320,7 @@ class _AirplanesMapState extends State<AirplanesMap> {
               ),
               SizedBox(height: 24),
 
-              // Destination Text Field
+              // Destination Field
               TextField(
                 decoration: InputDecoration(
                   labelText: 'Destination',
@@ -254,33 +329,29 @@ class _AirplanesMapState extends State<AirplanesMap> {
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
                     borderSide: BorderSide(color: Color(0xFF002157)),
                   ),
-                  suffixIcon: Icon(Icons.location_on),
+                  suffixIcon: Icon(Icons.flight_land),
                 ),
               ),
+              SizedBox(height: 24),
             ],
           ),
         ),
         Flexible(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFF002157), width: 4),
+            ),
             child: FlutterMap(
               options: MapOptions(
-                center: LatLng(0, 0),
+                center: LatLng(48.8566, 2.3522),
                 zoom: zoomLevel,
-                onPositionChanged: (MapPosition pos, bool hasGesture) {
-                  setState(() {
-                    zoomLevel = pos.zoom ?? 2.0;
-                  });
-                },
               ),
               children: [
                 TileLayer(
                   urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                   subdomains: ['a', 'b', 'c'],
                 ),
-                MarkerLayer(
-                  markers: airplaneMarkers,
-                ),
+                MarkerLayer(markers: airplaneMarkers),
               ],
             ),
           ),
